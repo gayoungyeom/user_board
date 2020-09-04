b5c07aeec8f3cb2a851752f6d68babbca44b47bf
gayoungyeom <gayoungyeom@gmail.com>
2020-09-04 19:11:23 +0900

    [Update] Post model
    Delete imageField in Post model

--- boardproject/userboard/models.py
+++ boardproject/userboard/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=50) # 제목
-    image = models.ImageField() # 이미지
     description = models.TextField() # 본문
     pub_date = models.DateTimeField()
 
