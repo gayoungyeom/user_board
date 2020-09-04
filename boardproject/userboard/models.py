7573f248ed62eb582619cd5aad099cdf88b0f892
gayoungyeom <gayoungyeom@gmail.com>
2020-09-04 13:47:10 +0900

    [Create] Post model

--- boardproject/userboard/models.py
+++ boardproject/userboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50) # 제목
+    image = models.ImageField() # 이미지
+    description = models.TextField() # 본문
+    pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.description[:20]
